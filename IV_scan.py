c4412f6b5e77c387cd0d399b6d4e44a4b4e97787
niko <niko@silab32>
2017-01-12 14:37:00 +0100

    test

--- IV_scan.py
+++ IV_scan.py
@@ -1,2 +1,18 @@
 import os
 import csv
+from tsv_scans.measurement_loop import IV as iv
+
+
+
+if __name__ == '__main__':
+    
+    voltage = 70
+    climit = 0.00001
+    stepsize = 1
+    
+    device = '/home/niko/git/LfPassiveCMOS/devices.yaml'
+    workdir = '/media/niko/data/LfPassiveCMOS/test'
+    
+    
+    iv(device).scan_tsv_res_VOLT(workdir, voltage, climit, 5000, stepsize)
+    
