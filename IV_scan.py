dcd92032d649fe2867704eec43b7e84866096233
niko <niko@silab32>
2017-01-13 14:28:59 +0100

    now scanning and plotting

--- IV_scan.py
+++ IV_scan.py
@@ -1,18 +1,34 @@
 import os
 import csv
-from tsv_scans.measurement_loop import IV as iv
+
+from IV_loop import IV
+
+from tsv_scans.playing_with_plots import TSV_res_meas_analysis as TSV
+from plot import LfCMOSplot as LF
+from uncertainties import ufloat, unumpy as unp
+
+from basil.dut import Dut
 
 
 
 if __name__ == '__main__':
     
-    voltage = 70
-    climit = 0.00001
-    stepsize = 1
+    voltage = 10
+    climit = 1e-5
+    stepsize = 1  
+    device_path = '/home/niko/git/LfPassiveCMOS/devices.yaml'
     
-    device = '/home/niko/git/LfPassiveCMOS/devices.yaml'
+    dut = Dut(device_path)
+    dut.init()
     workdir = '/media/niko/data/LfPassiveCMOS/test'
+    os.chdir(workdir)
+    f = 'test.csv'
     
+    devices = {'central':dut['Sourcemeter1']} #,'metal':dut['Sourcemeter2'],'backside':dut['Sourcemeter3']}
     
-    iv(device).scan_tsv_res_VOLT(workdir, voltage, climit, 5000, stepsize)
-    
+    print devices['central'].get_name()
+#     iv = IV(devices = devices, max_current = 15e-5)
+#     iv.scan_tsv_res_VOLT(f, voltage, climit, 5000, stepsize)
+    t = LF()
+    d = t.load_file(os.path.join(workdir,f))
+    t.plot_IV_curve(d[0],d[1])
