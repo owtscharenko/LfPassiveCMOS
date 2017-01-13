48b422561f836ce496b120bf2132e95157e5a8dd
niko <niko@silab32>
2017-01-13 14:28:07 +0100

    initial commit

---
+++ IV_loop.py
@@ -0,0 +1,184 @@
+import time
+import logging
+import csv
+# import matplotlib.pyplot as plt
+# import multiprocessing
+import progressbar
+import os.path
+
+#from scan_misc import Misc 
+# from basil.dut import Dut
+
+
+class IV(object):
+    
+    
+    def __init__(self, devices, max_current, minimum_delay=0.1):
+
+        self.max_current = max_current
+        self.minimum_delay = minimum_delay
+        self.devices = devices
+        self.data = []
+
+    
+    def get_device_type(self, device):
+        type_arr = device.get_name().split(' ')
+        for i, t in enumerate(type_arr):
+            if t == u'KEITHLEY':
+                vendor = 'keithley'
+            if u'MODEL' in t:
+                model = type_arr[i+1].split(',')[0]
+        if not vendor or not model:
+            raise RuntimeError('Could not determine device type!')
+        typ = vendor + '_' + model
+        return typ    
+    
+    
+    def get_voltage_reading(self, device):
+        typ = self.get_device_type(device)
+        if typ == 'keithley_2410':
+            return float(device.get_voltage().split(',')[0])
+        elif typ == 'keithley_6517A':
+            return float(device.get_voltage().split(',')[0][:-4])
+        else:
+            return float(device.get_voltage().split(',')[0])
+    
+
+    def get_current_reading(self, device):
+        typ = self.get_device_type(device)
+        if typ == 'keithley_2410':
+            return float(device.get_current().split(',')[1])
+        elif typ == 'keithley_6517A':
+            return float(device.get_current().split(',')[0][:-4])
+        else:
+            return float(device.get_current().split(',')[1])
+    
+    def measure_current(self, device, timeout):
+        
+#         was_above = False
+#         was_below = False
+        
+        current = float(device.get_current().split(',')[1])
+        for i in range(timeout):
+            time.sleep(self.minimum_delay)
+            measurement = self.get_current_reading(device)
+            if measurement < 1e-10 and i > 10:
+                break
+            if abs(abs(measurement) / abs(current) -1) <= 0.0001:
+#                 print abs(abs(measurement) / abs(current) -1)
+                break
+#             if abs(measurement) > abs(current):
+#                 was_above = True
+#             if abs(measurement) < abs(current):
+#                 was_below = True
+#             if was_above and was_below:
+#                 break
+#             if round(measurement,7) == round(current,7):
+#                 break
+            current = measurement
+        if i > 0 :
+            print 'current cycles %i' %i     
+        return measurement
+
+
+    def measure_voltage(self, device, timeout):
+        
+#         was_above = False
+#         was_below = False
+        
+        voltage = float(device.get_voltage().split(',')[0])
+        for i in range(timeout):
+            time.sleep(self.minimum_delay)
+            measurement = self.get_voltage_reading(device)
+            if abs(abs(measurement) / abs(voltage) -1) <= 0.0001:
+#                 print abs(abs(measurement) / abs(voltage) -1)
+                break
+#             if abs(measurement) > abs(voltage):
+#                 was_above = True
+#             if abs(measurement) < abs(voltage):
+#                 was_below = True
+#             if was_above and was_below:
+#                 break
+#             if round(measurement,7) == round(voltage,7):
+#                 break
+            voltage = measurement
+        if i > 0:
+            print 'voltage cycles %i' %i    
+        return measurement
+
+
+    def ramp_down(self):
+        logging.info('Ramping down all voltages...')
+        done = {}
+        for key in self.devices.keys():
+            done[key] = False
+            
+        while True:
+            for key in self.devices.keys():
+                if self.get_device_type(self.devices[key]) == 'keithley_6517A':
+                    done[key] = True
+                    continue
+                value = int(self.get_voltage_reading(self.devices[key]))
+                if value != 0.0:
+                    step = -1 if (value > 0) else 1
+                    self.devices[key].set_voltage(value + step)
+                else:
+                    done[key] = True
+            time.sleep(0.5)
+            if all([dev for dev in done.itervalues()]):
+                break
+
+
+    def reset(self):
+        self.data = []
+        self.ramp_down()
+        time.sleep(self.minimum_delay)
+
+
+    def scan_tsv_res_VOLT(self, file_name, max_Vin, stepsize, *device):
+        try:
+            self.reset()  
+            logging.info("Starting ...")              
+#             self.devices['central'].on()    
+            fncounter=1                                                 #creates output .csv
+            while os.path.isfile( file_name):
+                file_name = file_name.split('.')[0]
+                file_name = file_name.split('_')[0]
+                file_name = file_name + "_" + str(fncounter) + ".csv"
+                fncounter = fncounter + 1
+                              
+            pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=max_Vin, poll=10, term_width=80).start()               #fancy progress bar
+            with open(file_name , 'wb') as outfile:
+                f = csv.writer(outfile ,quoting=csv.QUOTE_NONNUMERIC)
+                    
+                f.writerow(['Input voltage [V]', 'Input current [A]'])           #What is written in the output file
+                                                                                     
+                for x in range(0, max_Vin+1):                          #loop over steps                                                                
+                    self.devices['central'].set_voltage(x)          #Set input current
+                    self.devices['central'].on()                             
+                    input_voltage = self.measure_voltage(self.devices['central'], 100)
+                    #self.dut['Sourcemeter'].on()
+                    input_current = self.measure_current(self.devices['central'], 100)    
+                    logging.info("Set input voltage to %r" % x)
+                    logging.info("Input current is %r A" % input_current)                              #Logging the readout
+                    logging.info("Input voltage is %r V" % input_voltage)
+#                     Writing readout in output file
+#                     if input_current > 0 :
+                    self.data.append([input_voltage, input_current])                   #Writing readout in output file
+                    f.writerow(self.data[-1])
+#                     elif input_current < 0:
+#                         print'warning: outside range! current = %f' % input_current
+                    pbar.update(x)                                                  #Increase input current for next iteration   
+                    if input_current >= self.max_current:
+                        print 'reached current limit of %f A! aborting' %self.max_current                                                                                                #Maximum values reached?
+                        break
+                pbar.finish()
+                self.ramp_down()
+                self.devices['central'].off()
+                logging.info('Measurement finished, plotting ...')
+                
+        except Exception as e:
+            logging.error(e)
+            self.ramp_down()
+            
+        return file_name
---
+++ plot.py
@@ -0,0 +1,85 @@
+import csv
+import os
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from uncertainties import ufloat, unumpy as unp
+from scipy.optimize import curve_fit
+
+from tsv_scans.playing_with_plots import TSV_res_meas_analysis as TSV
+
+
+
+class LfCMOSplot(object):
+    
+    def __init__(self):
+        self.TSV = TSV()
+        self.outformat = 'pdf'
+    
+    def load_file(self,path):
+            if not os.path.split(path)[1].split('.')[1] == 'csv':
+                raise IOError('Wrong filetype!')
+            self.dirpath = os.path.split(path)[0]
+            self.outfile = os.path.join(os.path.split(path)[0], (os.path.split(path)[1].split('.')[0]))
+            self.title = os.path.split(path)[1].split('.')[0]
+            
+            self.f = os.path.split(path)[1]    
+                
+            
+            x,y =     [], []
+            xerr = 0.0002 + 0.0005
+            yerr = 0.0005 + 0.0005
+            xrel = 0.0012
+            yrel = 0.003
+            with open(path, 'rb') as datafile:
+                linereader = csv.reader(datafile, delimiter=',', quotechar='"')
+                _ = linereader.next()
+                frow = linereader.next()
+                
+                ''' introducing syst. uncertainties here: voltage: 0.12 % + 200microV , current: 0.3 % + 500microA
+                    for std_dev one stepsize (500micro and 500microA) has been estimated
+                '''
+    
+                x.append(ufloat(frow[0], float(frow[0])*xrel + xerr))
+                y.append(ufloat(frow[1], float(frow[1])*yrel + yerr))
+                for row in linereader:
+                    x.append(ufloat(row[0], float(row[0])*xrel + xerr))
+                    y.append(ufloat(row[1], float(row[1])*yrel + yerr))
+    
+            return x, y
+        
+    
+    def plot_IV_curve(self,x,y):
+
+        voltage, voltage_std_dev = self.TSV.unpack_uncertainties(x)
+        current, current_std_dev = self.TSV.unpack_uncertainties(y)
+        
+        p, covariance =  curve_fit(self.TSV.fitfunction_line, voltage, current, p0=(0,0))
+        plt.cla()
+#         plt.xlim(0,0.2)
+#         plt.ylim(0,0.3)
+        plt.title(self.title + ' IV curve')
+        plt.ylabel('Current [A]')
+        plt.xlabel('Voltage [V]')
+#         plt.errorbar(x, y, yerr=e, fmt=None)
+#         box = AnchoredText('m = %.3f \n b = %.5f' %(p[0],p[1]), bbox_to_anchor=(1,1),loc=1)
+        textstr=('m = %.3f \n b = %.5f' %(p[0],p[1]))
+        ax = plt.axes()
+#         ax.text(0.1, round(((ax.get_ylim()[1] + ax.get_ylim()[0])/2), 0), textstr, bbox=dict(boxstyle='square', facecolor='white'))
+#         ax.add_artist(box)
+        plt.errorbar(voltage,current, voltage_std_dev, current_std_dev, 'b.', markersize = 3, label = 'data')
+        plt.plot(self.TSV.fitfunction_line(voltage, *p),'r-', linewidth = 1.2, label = 'fit \n'+ textstr)
+        leg = plt.legend(loc = 'best', numpoints = 1)
+#         plt.draw()
+#         loc = leg.get_window_extent().inverse_transformed(ax.transAxes)
+#         loc = leg.get_frame().get_bbox().bounds
+#         print loc
+#         leg_height = loc.p1[1]-loc.p1[0]
+#         print leg_height
+#         textbox = ax.text(-loc.p0[0]-0.05,-loc.p0[1]-0.05, textstr, bbox=dict(boxstyle='square', facecolor='white'))
+        print 'covariance: %r' % (np.sqrt(np.diag(covariance)))
+        print 'fit: %r' % p
+        
+        plt.savefig(self.outfile + '-IV-fit.'+ self.outformat)
+        plt.show()
