0e30b28555e9aac04457cf828cd2c652bb42d198
niko <niko@silab32>
2017-02-24 11:26:27 +0100

    changed function and parameter call to be in init

--- IV_loop.py
+++ IV_loop.py
@@ -5,7 +5,9 @@ import csv
 # import multiprocessing
 import progressbar
 import os.path
+import numpy as np
 
+from pybar.daq.fei4_raw_data import open_raw_data_file
 #from scan_misc import Misc 
 # from basil.dut import Dut
 
@@ -13,13 +15,18 @@ import os.path
 class IV(object):
     
     
-    def __init__(self, devices, max_current, minimum_delay=0.5):
+    def __init__(self, devices, filename, polarity, max_current, min_Vin, max_Vin, stepsize, minimum_delay):
 
         self.max_current = max_current
         self.minimum_delay = minimum_delay
         self.devices = devices
         self.data = []
-
+        self.max_Vin = max_Vin
+        self.min_Vin = min_Vin
+        self.stepsize = stepsize
+        self.file_name = filename
+        self.polarity = polarity
+        self.voltages = np.arange(min_Vin, max_Vin, stepsize).tolist()
     
     def get_device_type(self, device):
         type_arr = device.get_name().split(' ')
@@ -146,25 +153,25 @@ class IV(object):
         time.sleep(self.minimum_delay)
 
 
-    def scan_IV(self, file_name, min_Vin, max_Vin, polarity, stepsize, *device):
+    def scan_IV(self):
         try:
             self.reset()  
             logging.info("Starting ...")              
 #             self.devices['central'].on()    
             fncounter=1                                                 #creates output .csv
-            while os.path.isfile( file_name):
-                file_name = file_name.split('.')[0]
-                file_name = file_name.split('_')[0]
-                file_name = file_name + "_" + str(fncounter) + ".csv"
+            while os.path.isfile( self.file_name):
+                self.file_name = self.file_name.split('.')[0]
+                self.file_name = self.file_name.split('_')[0]
+                self.file_name = self.file_name + "_" + str(fncounter) + ".csv"
                 fncounter = fncounter + 1
                               
-            pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=max_Vin, poll=10, term_width=80).start()               #fancy progress bar
-            with open(file_name , 'wb') as outfile:
+            pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=self.max_Vin, poll=10, term_width=80).start()               #fancy progress bar
+            with open(self.file_name , 'wb') as outfile:
                 f = csv.writer(outfile ,quoting=csv.QUOTE_NONNUMERIC)
                 f.writerow(['Input voltage [V]', 'Input current [A]'])           #What is written in the output file
                 
-                for x in range(min_Vin, max_Vin+1, stepsize):
-                    input_voltage = x*polarity                     #loop over steps                                                                
+                for x in range(self.min_Vin, self.max_Vin+1, self.stepsize):
+                    input_voltage = x*self.polarity                     #loop over steps                                                                
                     self.devices['central'].set_voltage(input_voltage)
                     time.sleep(self.minimum_delay)          #Set input current
                     self.devices['central'].on()
@@ -187,44 +194,45 @@ class IV(object):
             logging.error(e)
             self.ramp_down()
             
-        return file_name
+        return self.file_name
     
-    def scan_IV_small_steps(self, file_name, max_Vin, polarity, steps, stepsize, *device):
+    def scan_IV_small_steps(self):
         try:
             self.reset()  
             logging.info("Starting ...")              
 #             self.devices['central'].on()    
             fncounter=1                                                 #creates output .csv
-            while os.path.isfile( file_name):
-                file_name = file_name.split('.')[0]
-                file_name = file_name.split('_')[0]
-                file_name = file_name + "_" + str(fncounter) + ".csv"
+            while os.path.isfile( self.file_name):
+                self.file_name = self.file_name.split('.')[0]
+                self.file_name = self.file_name.split('_')[0]
+                self.file_name = self.file_name + "_" + str(fncounter) + ".csv"
                 fncounter = fncounter + 1
                               
-            pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=max_Vin, poll=10, term_width=80).start()               #fancy progress bar
-            with open(file_name , 'wb') as outfile:
+            pbar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=self.max_Vin, poll=10, term_width=80).start()               #fancy progress bar
+            with open(self.file_name , 'wb') as outfile:
                 f = csv.writer(outfile ,quoting=csv.QUOTE_NONNUMERIC)
                     
                 f.writerow(['Input voltage [V]', 'Input current [A]'])           #What is written in the output file
 #                 print self.devices['central'].set_autorange()
                 input = 0                                
-                for x in range(0, steps):         #loop over steps                                                             
-                    self.devices['central'].set_voltage(input*polarity)          #Set input current
+                for x in range(0, 500000):
+                    input_voltage =  input*self.polarity        #loop over steps                                                             
+                    self.devices['central'].set_voltage(input_voltage)          #Set input current
                     self.devices['central'].on()
-                    input_current = self.measure_current(self.devices['central'], 100)                             
-                    input_voltage = self.measure_voltage(self.devices['central'], 100)
-                    logging.info("Set input voltage to %r V" % input*polarity)
+                    input_current = self.measure_current(self.devices['central'], 50)                             
+#                     input_voltage = self.measure_voltage(self.devices['central'], 100)
+                    logging.info("Set input voltage to %r V" % input*self.polarity)
                     logging.info("Input current is %r A" % input_current)                              #Logging the readout
                     logging.info("Input voltage is %r V" % input_voltage)
 #                     Writing readout in output file
                     self.data.append([input_voltage, input_current])                   #Writing readout in output file
                     f.writerow(self.data[-1])
                     pbar.update(input)
-                    input += stepsize  
+                    input += self.stepsize  
                     if abs(input_current) >= self.max_current:
                         print 'reached current limit of %f A! aborting' %self.max_current                                                                                                #Maximum values reached?
                         break
-                    elif input >= max_Vin:
+                    elif input >= self.max_Vin:
                         print 'reached maximum Voltage of %f V' % input
                         break
                 pbar.finish()
@@ -236,4 +244,52 @@ class IV(object):
             logging.error(e)
             self.ramp_down()
             
-        return file_name
+        return self.file_name
+    
+    
+    def scan_IV_h5(self):
+        logging.info('Measure IV for V = %s' % self.voltages)
+        description = [('voltage', np.float), ('current', np.float)]
+        data = open_raw_data_file.h5_file.create_table(self.raw_data_file.h5_file.root, name='IV_data', description=np.zeros((1, ), dtype=description).dtype, title='Data from the IV scan')
+
+        progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=len(self.voltages), term_width=80)
+        progress_bar.start()
+
+        for index, voltage in enumerate(self.voltages):
+            if self.stop_run.is_set():
+                break
+#             if voltage > 0:
+#                 RuntimeError('Voltage has to be negative! Abort to protect device.')
+            if self.abort_run.is_set():
+                break
+            if abs(voltage) <= abs(self.max_Vin):
+                self.dut['Sourcemeter'].set_voltage(voltage)
+                self.actual_voltage = voltage
+                time.sleep(self.minimum_delay)
+            else:
+                logging.info('Maximum voltage with %f V reached, abort', voltage)
+                break
+            current_string = self.dut['Sourcemeter'].get_current()
+            current = float(current_string.split(',')[1])
+            if abs(current) > abs(self.max_current):
+                logging.info('Maximum current with %e I reached, abort', current)
+                break
+            logging.info('V = %f, I = %e', voltage, current)
+            max_repeat = 50
+            for i in range(max_repeat):  # repeat current measurement until stable (current does not increase)
+                time.sleep(self.minimum_delay)
+                actual_current = float(self.dut['Sourcemeter'].get_current().split(',')[1])
+                if abs(actual_current) > abs(self.max_current):
+                    logging.info('Maximum current with %e I reached, abort', actual_current)
+                    break
+                if (abs(actual_current) < abs(current)):  # stable criterion
+                    break
+                current = actual_current
+            if i == max_repeat - 1:  # true if the leakage always increased
+                raise RuntimeError('Leakage current is not stable')
+            else:
+                a = np.array([(voltage, current)], dtype=description)
+                data.append(a)
+            progress_bar.update(index)
+        progress_bar.finish()
+        data.flush()
--- IV_scan.py
+++ IV_scan.py
@@ -13,26 +13,28 @@ from basil.dut import Dut
 
 if __name__ == '__main__':
     
-    voltage = 10
+    min_vin = 0
+    max_vin = 50
     polarity = -1
-    climit = 1e-4
-    stepsize = 0.1 
+    climit = 1e-6
+    stepsize = 1 
     device_path = '/home/niko/git/LfPassiveCMOS/devices.yaml'
     
     dut = Dut(device_path)
     dut.init()
-    workdir = '/media/niko/data/LfPassiveCMOS/LFCMOS-x-4/IVscans'
+    workdir = '/media/niko/data/LfPassiveCMOS/LFCMOS-x-5/IVscans'
     os.chdir(workdir)
-    f = 'LFCMOS-x-4L-100mV-steps-nocover-' + str(voltage) + 'V.csv'
+    f = 'test' + str(max_vin) + 'testtest.csv'
     
     devices = {'central':dut['Sourcemeter']} #,'metal':dut['Sourcemeter2'],'backside':dut['Sourcemeter3']}
      
-    iv = IV(devices = devices, max_current = climit, minimum_delay= 1)
-#     iv.scan_IV(f, voltage,polarity, stepsize)
-    iv.scan_IV_small_steps(f, voltage,polarity,5000, stepsize)
+    iv = IV(devices, f, polarity, climit, min_vin, max_vin, stepsize, minimum_delay= 1)
+    filename = iv.scan_IV()
+#     filename = iv.scan_IV_small_steps()
+
 #    iv.ramp_to(devices['central'], -50)
 #    time.sleep(5)
 #    iv.ramp_down()
     t = LF()
-    d = t.load_file(os.path.join(workdir,f))
+    d = t.load_file(os.path.join(workdir,filename))
     t.plot_IV_curve(d[0],d[1],fit=False,logy=True)
